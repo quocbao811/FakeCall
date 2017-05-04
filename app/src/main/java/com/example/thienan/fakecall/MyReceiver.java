9da9596bb56a9cd523b51ef2b6fc2f810e4d7fed
quocbao811 <quoc1340811@gmail.com>
2017-05-04 16:24:30 +0700

    - First Push

---
+++ app/src/main/java/com/example/thienan/fakecall/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.thienan.fakecall;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ThienAn on 5/4/2017.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
